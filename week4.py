f4c124e22dd3a67b722fb72588624c8d1d20994a
Elaine Wong <116803728+Elaine-WWY@users.noreply.github.com>
2024-09-19 00:34:32 +0800

    Update week4.py

--- week4.py
+++ week4.py
@@ -19,9 +19,11 @@
 # import the library
 from sklearn.model_selection import train_test_split
 from sklearn import linear_model
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 import math
 import pandas as pd
+import numpy as np
 
 
 #Question1
@@ -31,7 +33,7 @@ def load():
 	return df
 	'''
 	#SOLUTION START( ~ 1 line of code)
-	
+	df = pd.read_csv("data/realestate.csv")
 
 	#SOLUTION END
 	return df
@@ -52,7 +54,7 @@ def getShape():
 	return the shape of dataframe
 	'''
 	#SOLUTION START( ~ 1 line of code)
-	
+	return df.shape
 
 	#SOLUTION END
 
@@ -65,7 +67,7 @@ def getInfo():
 	including the index dtype and columns, non-null values and memory usage.
 	'''
 	#SOLUTION START( ~ 1 line of code)
-	
+	return df.info()
 
 	#SOLUTION END
 
@@ -88,7 +90,7 @@ def checkNull():
 	'''
 
 	#SOLUTION START( ~ 1 line of code)
-	
+	return df.isnull().sum()
 
 	#SOLUTION END
 
@@ -102,7 +104,7 @@ def getStatistic():
 	'''
 
 	#SOLUTION START( ~ 1 line of code)
-
+	return df.describe()
 	#SOLUTION END
 
 print(getStatistic())
@@ -126,14 +128,15 @@ def split():
 	IMPORTANT: this function will return four values X_train, X_test, y_train, y_test
 	'''
 	#SOLUTION START( ~ 1-2 line of code)
-
+	X_train, X_test, y_train, y_test = train_test_split(df_X, df_y, test_size=0.2, random_state=42)
+	return X_train, X_test, y_train, y_test
 	#SOLUTION END
 
 X_train, X_test, y_train, y_test = split()
 
 
 #Question 9
-def makeModel():
+def makeModel(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test):
 	'''
 	this function will create linear regression object,train the model using the training sets,
 	make predictions using the testing set
@@ -142,28 +145,28 @@ def makeModel():
 
 	# Create linear regression object
 	#SOLUTION START(~ 1 line of code)
-
+	model = LinearRegression()
 	#SOLUTION END
 
 	# Train the model using the training sets
 	#SOLUTION START(~ 1 line of code)
-
+	model.fit(X_train, y_train)
 
 	#SOLUTION END
 
 	# Make predictions using the testing set
 	#SOLUTION START(~ 1 line of code)
-
+	y_pred = model.predict(X_test)
 	#SOLUTION END
 
 	#fill in the blanks, how to get the value of coefficients, intercept, mse, rmse, r2score
 	#SOLUTION START
 	#The coefficients i.e. the slope
-	coefficients = #YOUR ANSWER
-	intercept = #YOUR ANSWER
-	mse = #YOUR ANSWER
-	rmse = #YOUR ANSWER
-	r2score = #YOUR ANSWER
+	coefficients = model.coef_
+	intercept = model.intercept_
+	mse = mean_squared_error(y_test, y_pred)
+	rmse = np.sqrt(mse) 
+	r2score = r2_score(y_test, y_pred)
 	#SOLUTION END
 
 
